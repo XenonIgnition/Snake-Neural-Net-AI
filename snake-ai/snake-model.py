023d8504ff0d2d0125778ab31ae54d5f1080d945
Xenon <femiadesina@live.co.uk>
2018-09-03 09:40:03 +0100

    First actual commit. Added files to create container for snake model

---
+++ snake-ai/snake-model.py
@@ -0,0 +1,12 @@
+"""
+Company: XenonIgnition
+Author: Adesina Femi
+Date: 03/09/2018
+Script: snake-model.py
+
+
+This module will house the first attempt at creating a bot that is capable of playing the snake game. This will enable
+me to have better experience at what a model will require to play a game when I want to create a game ai.
+"""
+
+
